800b9cff296c7c58599516a3644553ca5834d076
manikmmalhotra <manikmalhotra2113@gmail.com>
2020-09-08 23:09:41 +0530

    firebase added

--- app/src/main/java/com/jerry/calculator/imageActivity.kt
+++ app/src/main/java/com/jerry/calculator/imageActivity.kt
@@ -1,11 +1,21 @@
 package com.jerry.calculator
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_image.*
+
 
 class imageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image)
+
+        addbtn.setOnClickListener {
+            val intent = Intent()
+            intent.type = "image/*"
+            intent.action = Intent.ACTION_GET_CONTENT
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), 1)
+        }
     }
 }
